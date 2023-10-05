84da5e7fa2727170fa2a0c7f01d1dd684313dff9
Evie Hoang <evie.h0325@gmail.com>
2023-10-04 22:47:11 -0700

    Added time in html and javascript. Refresh time every 1 second.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+// Variables
+var day = $('#currentDay');
+var saveBtn = document.getElementById("saveBtn");
+
+
+
+setInterval (function showDate(){
+  var today = dayjs()
+  day.text(today);
+})
+
+showDate();
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
